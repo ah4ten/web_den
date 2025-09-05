c86dc7bca704786367d2b68069d471f20908b448
ah4ten <194135785+ah4ten@users.noreply.github.com>
2025-09-05 16:29:36 +0700

    Create _store.js

---
+++ api/_store.js
@@ -0,0 +1,22 @@
+// api/_store.js
+// simple shared memory store for serverless instance lifecycle
+const store = {
+  command: {
+    mode: "manu",
+    lamp1: 0,
+    lamp2: 0,
+    bright1: 255,
+    bright2: 255
+  },
+  status: {
+    lamp1: 0,
+    lamp2: 0,
+    bright1: 0,
+    bright2: 0,
+    pir1: 0,
+    pir2: 0,
+    lastSeen: null
+  }
+};
+
+export default store;
