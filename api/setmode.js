160a1f7564f04c7e9610301d6a4ca82adeb70f8b
ah4ten <194135785+ah4ten@users.noreply.github.com>
2025-09-05 17:24:16 +0700

    Update setmode.js

--- api/setmode.js
+++ api/setmode.js
@@ -4,6 +4,8 @@ import store from "./_store";
 export default function handler(req, res) {
   const m = req.query.m || (req.method === "POST" && req.body.m);
   if (!m) return res.status(400).send("Missing parameter m");
+
+  // Khi đổi mode -> tắt tất cả đèn (y như yêu cầu)
   store.command.lamp1 = 0;
   store.command.lamp2 = 0;
 
