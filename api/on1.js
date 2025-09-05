5dccb466508963931a23b42c2964a0ff117081f4
ah4ten <194135785+ah4ten@users.noreply.github.com>
2025-09-05 17:22:09 +0700

    Update on1.js

--- api/on1.js
+++ api/on1.js
@@ -1,6 +1,7 @@
-// api/off1.js
+// api/on1.js
 import store from "./_store";
 export default function handler(req, res) {
-  store.command.lamp1 = 0;
+  store.command.lamp1 = 1;
+  store.command.bright1 = 255; // bật mặc định max
   return res.status(200).send("OK");
 }
