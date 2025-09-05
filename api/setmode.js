f7f5ea39edab3201df427b13d7a485cb5b3a9833
ah4ten <194135785+ah4ten@users.noreply.github.com>
2025-09-05 17:23:54 +0700

    Update setmode.js

--- api/setmode.js
+++ api/setmode.js
@@ -2,12 +2,14 @@
 import store from "./_store";
 
 export default function handler(req, res) {
-  const m = req.query.m || (req.method === 'POST' && req.body.m);
-  if (!m) return res.status(400).send("Missing m");
-  // when change mode, turn off all lamps (match your ESP behavior)
+  const m = req.query.m || (req.method === "POST" && req.body.m);
+  if (!m) return res.status(400).send("Missing parameter m");
   store.command.lamp1 = 0;
   store.command.lamp2 = 0;
-  if (m === 'manu') store.command.mode = 'manu';
-  else if (m === 'auto') store.command.mode = 'auto';
+
+  if (m === "manu") store.command.mode = "manu";
+  else if (m === "auto") store.command.mode = "auto";
+  else return res.status(400).send("Invalid mode");
+
   return res.status(200).send("OK");
 }
