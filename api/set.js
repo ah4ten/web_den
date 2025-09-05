3be6e3da7cc177760a7dcfeca32e5e131a1884fb
ah4ten <194135785+ah4ten@users.noreply.github.com>
2025-09-05 17:23:36 +0700

    Update set.js

--- api/set.js
+++ api/set.js
@@ -2,15 +2,17 @@
 import store from "./_store";
 
 export default function handler(req, res) {
-  const lamp = req.query.lamp || (req.method === 'POST' && req.body.lamp);
-  const value = req.query.value || (req.method === 'POST' && req.body.value);
+  const lamp = req.query.lamp || (req.method === "POST" && req.body.lamp);
+  const value = req.query.value || (req.method === "POST" && req.body.value);
 
   if (!lamp || value === undefined) {
     return res.status(400).send("Missing parameter");
   }
   const l = parseInt(lamp);
-  const v = parseInt(value);
-  if (l === 1) store.command.bright1 = Math.max(0, Math.min(255, v));
-  else if (l === 2) store.command.bright2 = Math.max(0, Math.min(255, v));
+  const v = Math.max(0, Math.min(255, parseInt(value)));
+  if (l === 1) store.command.bright1 = v;
+  else if (l === 2) store.command.bright2 = v;
+  else return res.status(400).send("Invalid lamp");
+
   return res.status(200).send("OK");
 }
