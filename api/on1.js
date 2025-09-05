b52f1964fe29ad6da1b3b76f678e0f252580b538
ah4ten <194135785+ah4ten@users.noreply.github.com>
2025-09-05 17:21:17 +0700

    Update on1.js

--- api/on1.js
+++ api/on1.js
@@ -1,9 +1,7 @@
 // api/on1.js
 import store from "./_store";
-
 export default function handler(req, res) {
-  // emulate original ESP route behavior: update command: lamp1 = 1, bright1 stays (but set to 255 on ON in ESP)
   store.command.lamp1 = 1;
-  store.command.bright1 = 255;
+  store.command.bright1 = 255; // bật mặc định max
   return res.status(200).send("OK");
 }
